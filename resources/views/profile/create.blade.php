1589de69ea6c99937e4d7b0de52929de0a81bfef
abutoku <ystkz2021@gmail.com>
2022-03-09 15:50:49 +0900

    update register

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -1,43 +1,41 @@
-{{-- ヘッダー要素・コンポーネント ⏬⏬--}}
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Create') }}
-        </h2>
-    </x-slot>
-{{-- ヘッダー要素・コンポーネント ⏫⏫--}}
+<x-guest-layout>
+    {{-- 新規登録→プロフィール登録画面 --}}
 
-    {{-- -----プロフィールの登録画面----- --}}
-        <div class="flex justify-center">
-    <div class="px-2 mx-2 my-4 rounded-lg shadow-lg bg-white max-w-sm w-full">
-    <h1 class="text-lg font-bold">add profile</h1>
-    {{-- -----プロフィール入力フォーム----- --}}
-    <form action="{{ route('profile.store') }}" method="post" enctype="multipart/form-data" class="pb-5">
-        @csrf
+    <x-auth-card>
 
-        <div>
-            <p>card rank</p>
-            {{-- カードの種類 --}}
-            <select name="card_rank">
-                <option value="Pro">Pro</option>
-                <option value="DM">DM</option>
-                <option value="MSD">MSD</option>
-                <option value="AOW">AOW</option>
-                <option value="OW">OW</option>
-            </select>
-        </div>
+        {{-- ロゴ表示部分 --}}
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo/>
+            </a>
+        </x-slot>
 
-        <div class="my-2">
-            {{-- 今まで潜った本数 --}}
-            <p>dive count</p>
-            <input type="number" name="dive_count">
-        </div>
+        {{-- プロフィール入力フォーム --}}
+        <form action="{{ route('profile.store') }}" method="post" enctype="multipart/form-data"
+        class="p-6">
+            @csrf
+                <div class="ml-14">
+                    <p class="text-divenavy text-sm">カードランク</p>
+                    {{-- カードの種類 --}}
+                    <select name="card_rank" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-divenavy">
+                        <option value="Pro">Pro</option>
+                        <option value="DM">DM</option>
+                        <option value="MSD">MSD</option>
+                        <option value="AOW">AOW</option>
+                        <option value="OW">OW</option>
+                    </select>
+                </div>
 
-        <x-button class="mb-3">add</x-button>
-    </form>
+                <div class="my-6 ml-14">
+                    {{-- 今まで潜った本数 --}}
+                    <p class="text-divenavy text-sm" >ダイブ本数</p>
+                    <input type="number" name="dive_count" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    DIVE
+                </div>
 
-    </div>
-</div>
-    {{-- -----プロフィール入力フォームここまで----- --}}
 
-</x-app-layout>
+            <x-button class="mt-4 ml-64">登録</x-button>
+        </form>
+
+    </x-auth-card>
+</x-guest-layout>
