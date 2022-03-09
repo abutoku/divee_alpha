f2508ad46b1145ddec8ae6181952addc234c8742
abutoku <ystkz2021@gmail.com>
2022-03-09 13:19:50 +0900

    update welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,25 +13,40 @@
 
 </head>
 <body>
-    {{-- ロゴマークの読み込み --}}
-    <div class="bg-blue-500">
+    {{-- 全体の背景 --}}
+    <div class="bg-diveblue h-[1200px]">
         {{-- wrapper --}}
-        <div class="bg-blue-600 w-11/12 mr-auto ml-auto">
+        <div class="w-11/12 pt-24 mr-auto ml-auto">
 
-            <div>
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            {{-- ロゴマークの読み込み --}}
+            <div class="flex justify-center">
+                <x-application-logo/>
             </div>
 
-            {{-- ログインリンク --}}
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
-
-
-            {{-- 新規登録リンク --}}
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">新規登録はこちら</a>
-
-        </div>{{-- wrapperここまで --}}
+            {{-- ログインボタン部分 --}}
+                @auth
+                {{-- すでにログイン済みであればHOMEへ --}}
+                <div class="mt-24 flex justify-center">
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 no-underline">
+                    <div class="mb-4  text-diveblue bg-white w-[200px] h-12 rounded-xl flex justify-center items-center">HOMEへ</div>
+                    </a>
+                </div>
+                @else
+                {{-- ログインリンク --}}
+                    <div class="mt-24 flex justify-center">
+                        <a href="{{ route('login') }}">
+                            <div class="mb-4  text-diveblue bg-white w-[200px] h-12 rounded-xl flex justify-center items-center">ログイン</div>
+                        </a>
+                    </div>
+                {{-- 新規登録リンク --}}
+                    <div class="flex justify-center">
+                        <a href="{{ route('register') }}" class="ml-20 text-sm text-white dark:text-gray-500 underline">
+                        新規登録はこちら</a>
+                    </div>
+                @endauth
+        </div>
+        {{-- wrapperここまで --}}
     </div>
-
-
+    {{-- 全体ここまで --}}
 </body>
 </html>
