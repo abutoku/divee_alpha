a848849e36b3ab68282c0458d7d5dad05698329d
abutoku <ystkz2021@gmail.com>
2022-03-15 14:33:09 +0900

    update log controller

--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -22,6 +22,17 @@
                             <div class="mt-4">{{  $log->divesite }}</div>
                             <div>水深 : {{  $log->depth }}M</div>
                             <div>水温 : {{  $log->temp }}℃</div>
+                            {{-- 削除ボタン --}}
+                            <form action="{{ route('log.destroy',$log->id )}}" method="post">
+                                @method('delete')
+                                @csrf
+                                {{-- 削除ボタン --}}
+                                <button type="submit" class="mt-6">
+                                    <svg class="h-6 w-6 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                    </svg>
+                                </button>
+                            </form>
                         </div>
                         <div>
                             <img src="{{ Storage::url($log->image) }}" alt="picture" class="h-60 w-42 object-cover rounded-r-lg">
@@ -34,6 +45,17 @@
                         <div class="mt-4">{{  $log->divesite }}</div>
                         <div>水深 : {{  $log->depth }}M</div>
                         <div>水温 : {{  $log->temp }}℃</div>
+                        {{-- 削除ボタン --}}
+                            <form action="{{ route('log.destroy',$log->id )}}" method="post">
+                                @method('delete')
+                                @csrf
+                                {{-- 削除ボタン --}}
+                                <button type="submit" class="mt-6">
+                                    <svg class="h-6 w-6 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                    </svg>
+                                </button>
+                            </form>
                     </div>
                 @endif
             @endforeach
