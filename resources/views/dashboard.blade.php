cedc300c42a30e6f0ab3ff8dc115d7f69d02091e
abutoku <ystkz2021@gmail.com>
2022-03-27 23:58:36 +0900

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,7 +45,8 @@
             <div class="bg-white rounded-lg drop-shadow-md h-24 w-5/12 mb-6 flex justify-center items-center">QRコード</div>
         </div>
 
-        
+
         {!! QrCode::generate(route('post.index')) !!}
+        <a href="{{ route('buddy.index') }}">テスト</a>
     </section>
 </x-app-layout>
